51f1234cc51d38b7af9e430268a0e2fa9dd88f95
jyeonki <kay8825@naver.com>
2022-07-01 18:20:30 +0900

    [220701] java DB연동

--- sql_practice/pay_app.sql
+++ sql_practice/pay_app.sql
@@ -3,7 +3,7 @@
 --DROP SEQUENCE seq_pay;
  
 CREATE TABLE pay (
-    emp_num NUMBER(8),
+    emp_num CHAR(8),
     emp_name VARCHAR2(20) NOT NULL,
     emp_rank VARCHAR2(10) NOT NULL,
     base_pay NUMBER(8),
@@ -12,23 +12,45 @@ CREATE TABLE pay (
     CONSTRAINT pk_pay PRIMARY KEY (emp_num)
 );
 
+COMMENT ON TABLE pay IS '급여';
+COMMENT ON COLUMN pay.emp_num IS '사원번호';
+COMMENT ON COLUMN pay.emp_name IS '사원명';
+COMMENT ON COLUMN pay.emp_rank IS '직급';
+COMMENT ON COLUMN pay.base_pay IS '기본급';
+COMMENT ON COLUMN pay.tax_rate IS '세율';
+COMMENT ON COLUMN pay.net_salary IS '순급여';
+
+
 SELECT * FROM pay;
 
-CREATE SEQUENCE  seq_pay;
+
+CREATE SEQUENCE seq_pay;
 --    START WITH    
 --    INCREMENT BY 1
 
-INSERT INTO pay VALUES (to_char(sysdate,'yymmdd')|| + SEQ_PAY.nextval, '김사원', '사원', 2000000, 0.10, 1800000);
-INSERT INTO pay VALUES (to_char(sysdate,'yymmdd')|| + SEQ_PAY.nextval, '나대리', '대리', 2500000, 0.15, 2125000);
-INSERT INTO pay VALUES (to_char(sysdate,'yymmdd')|| + SEQ_PAY.nextval, '박과장', '과장', 3000000, 0.20, 2400000);
-INSERT INTO pay VALUES (to_char(sysdate,'yymmdd')|| + SEQ_PAY.nextval, '이부장', '부장', 3500000, 0.25, 2625000);
+INSERT INTO pay VALUES (TO_CHAR(sysdate,'yymmdd')|| + LPAD(seq_pay.nextval, 2, '0'), '김사원', '사원', 2000000, 0.10, 1800000);
+INSERT INTO pay VALUES (TO_CHAR(sysdate,'yymmdd')|| + LPAD(seq_pay.nextval, 2, '0'), '나대리', '대리', 2500000, 0.15, 2125000);
+INSERT INTO pay VALUES (TO_CHAR(sysdate,'yymmdd')|| + LPAD(seq_pay.nextval, 2, '0'), '박과장', '과장', 3000000, 0.20, 2400000);
+INSERT INTO pay VALUES (TO_CHAR(sysdate,'yymmdd')|| + LPAD(seq_pay.nextval, 2, '0'), '이부장', '부장', 3500000, 0.25, 2625000);
 
+
+--ROLLBACK;
 COMMIT;
+
 
 SELECT
     *
 FROM pay
-WHERE emp_num = 2206302
+WHERE emp_num = '22070101'
+;
+
+SELECT AVG(net_salary) AS avg_emp_pay 
+FROM pay;
+
+SELECT 
+    * 
+FROM pay
+ORDER BY emp_num
 ;
 
 
